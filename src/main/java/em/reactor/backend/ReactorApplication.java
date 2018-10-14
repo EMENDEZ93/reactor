8eeed9350e7c7c1a1d0413f36387725aaa4daa4d
- Edwin Mendez - <emendez@unac.edu.co>
2018-10-14 18:32:42 -0500

    Flux

--- src/main/java/em/reactor/backend/ReactorApplication.java
+++ src/main/java/em/reactor/backend/ReactorApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
 public class ReactorApplication implements CommandLineRunner {
 
@@ -13,6 +15,11 @@ public class ReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		Flux<String> programingLanguage = Flux.just("Java", "Python", "C#") 
+				.doOnNext( System.out::println );
+
+		programingLanguage.subscribe();
 		
 	}
 }
